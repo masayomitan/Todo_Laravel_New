04ef9c510239bc99eab1cbb4a59fa58d2285489c
kawasaki <yomitanmasa3523@gmail.com>
2020-04-28 13:22:56 +0900

    aaa

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -52,12 +53,29 @@ class TaskController extends Controller
         $task->title = $request->title;
         $task->due_date = $request->due_date;
 
-        $current_folder->task()->save($task);
+        $current_folder->taskss()->save($task);
 
         return redirect()->route('tasks.index',[
           'id' => $current_folder->id,
         ]);
     }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+{
+    // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+}
 
 
 }
